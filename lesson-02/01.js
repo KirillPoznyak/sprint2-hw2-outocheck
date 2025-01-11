59fc804460af2ee6523f829a1eb4390abb8d786b
kirill poznyak <k.paznyak@gmail.com>
2025-01-11 21:02:29 +0300

    Complete 1st task

---
+++ lesson-02/01.js
@@ -0,0 +1,27 @@
+/*
+* Создать программный код, который определяет, может ли пользователь получить доступ к ресурсу на основе его роли и наличия разрешений.
+* Полученное булево значение сохраните в переменной isAccess.
+
+* Условия доступа:
+* - Пользователь должен иметь статус администратора (isAdmin) или быть подтвержденным пользователем (isVerifiedUser)
+* - А также пользователь должен обладать специальным разрешением (hasSpecialPermission) или временным пропуском (hasTemporaryPass)
+*/
+
+// тестовые данные (значения можно менять)
+const isAdmin = false
+const isVerifiedUser = true
+const hasSpecialPermission = true
+const hasTemporaryPass = false
+
+let isAccess
+if((isAdmin===true && hasSpecialPermission===true) ||
+    (isAdmin===true && hasTemporaryPass===true) ||
+    (isVerifiedUser===true && hasSpecialPermission===true) ||
+    (isVerifiedUser===true && hasTemporaryPass===true)){
+    isAccess=true;
+    console.log(isAccess);
+} else {
+    isAccess=false;
+    console.log(isAccess);
+}
+// your code
